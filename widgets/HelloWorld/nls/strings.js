7aa2e761a12a27b37ab2de3aa8d89929580295b5
jcc <john.c.cartwright@comcast.net>
2019-02-02 14:08:14 -0700

    scaffold out ES2015 widget

---
+++ widgets/HelloWorld/nls/strings.js
@@ -0,0 +1,9 @@
+define({
+  root: {
+    _widgetLabel: 'Hello World',
+    widgetTitle: 'Hello World',
+    description: 'A custom Web AppBuilder widget.'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});
